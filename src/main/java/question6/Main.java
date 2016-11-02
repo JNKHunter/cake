cfa4612b4b181c8dc9b2d58a2849bdb70710ff08
John Hunter <john@pushagency.io>
2016-11-02 15:41:57 -0700

    fixed width/height math

--- src/main/java/question6/Main.java
+++ src/main/java/question6/Main.java
@@ -5,13 +5,13 @@ package question6;
  */
 public class Main {
     public static void main(String[] args) {
-        Rectangle rect1 = new Rectangle(10,10,10,10);
-        Rectangle rect2 = new Rectangle(15,15,10,10);
+        Rectangle rect1 = new Rectangle(10,10,5,5);
+        Rectangle rect2 = new Rectangle(12,13,7,5);
 
-        getIntersection(rect1, rect2);
+        System.out.println(getIntersection(rect1, rect2));
     }
 
-    private static void getIntersection(Rectangle rect1, Rectangle rect2){
+    private static Rectangle getIntersection(Rectangle rect1, Rectangle rect2){
         int rect1LeftEdge = rect1.leftX;
         int rect1RightEdge = rect1.leftX + rect1.width;
         int rect1TopEdge = rect1.bottomY + rect1.height;
@@ -22,12 +22,27 @@ public class Main {
         int rect2TopEdge = rect2.bottomY + rect2.height;
         int rect2BottomEdge = rect2.bottomY;
 
+        int rectIntersectLeft = 0;
+        int rectIntersectBottom = 0;
+        int rectIntersectWidth = 0;
+        int rectIntersectHeight = 0;
+
         if((rect1RightEdge >= rect2LeftEdge && rect2RightEdge >= rect1LeftEdge) &&
                 (rect1BottomEdge <= rect2TopEdge && rect1TopEdge >= rect2BottomEdge)){
 
             System.out.println("intersection!!");
+            int maxWidth = Integer.min(rect1.width, rect2.width);
+            int maxHeight = Integer.min(rect2.height, rect2.height);
+
+            rectIntersectWidth = maxWidth - (Math.abs(rect1.leftX - rect2.leftX));
+            rectIntersectHeight = maxHeight - (Math.abs(rect1.bottomY - rect2.bottomY));
+
+
+
 
 
         }
+
+        return new Rectangle(0,0,rectIntersectWidth,rectIntersectHeight);
     }
 }
