9f1894761e5ea39d9798460594b3c61e39be8360
John Hunter <john@pushagency.io>
2016-11-08 20:21:19 -0800

    o(n) solution

--- src/main/java/question1/Main.java
+++ src/main/java/question1/Main.java
@@ -4,4 +4,23 @@ package question1;
  * Created by John on 11/8/16.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        int[] stockPricesYesterday = new int[]{10, 7, 5, 8, 11, 9, 12, 1, 15};
+        System.out.println(getMaxProfit(stockPricesYesterday));
+    }
+
+    public static int getMaxProfit(int[] prices){
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for(int currentPrice: prices){
+            minPrice = Math.min(minPrice, currentPrice);
+            int potentialProfit = currentPrice - minPrice;
+            maxProfit = Math.max(maxProfit, potentialProfit);
+        }
+
+        return maxProfit;
+    }
+
 }
