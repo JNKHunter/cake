496bf596b964d8be9ad4ef9f8125abc2ff5d2340
John Hunter <john@pushagency.io>
2016-11-10 14:26:33 -0800

    o(n) time solution

--- src/main/java/question2/Main.java
+++ src/main/java/question2/Main.java
@@ -4,4 +4,34 @@ package question2;
  * Created by John on 11/10/16.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        int[] ints = {5, 7, 3, 4};
+        int[] products = getProductsOfAllIntsExceptAtIndex(ints);
+
+        for (int cur : products) {
+            System.out.println(cur);
+        }
+    }
+
+
+    public static int[] getProductsOfAllIntsExceptAtIndex(int[] intArray) {
+
+        int[] productsOfAllIntsExceptAtIndex = new int[intArray.length];
+
+        int productSoFar = 1;
+        for (int i = 0; i < intArray.length; i++) {
+            productsOfAllIntsExceptAtIndex[i] = productSoFar;
+            productSoFar *= intArray[i];
+        }
+
+        productSoFar = 1;
+        for (int i = intArray.length - 1; i >= 0; i--) {
+            productsOfAllIntsExceptAtIndex[i] *= productSoFar;
+            productSoFar *= intArray[i];
+        }
+
+        return productsOfAllIntsExceptAtIndex;
+    }
 }
+
