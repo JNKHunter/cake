134f8920a1f9e18a30bb93d9be1d9000dc0e0652
John Hunter <john@pushagency.io>
2016-11-24 14:07:56 -0800

    added tests

--- src/test/java/question11/UrlMapTest.java
+++ src/test/java/question11/UrlMapTest.java
@@ -3,20 +3,33 @@ package question11;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by John on 11/24/16.
  */
 public class UrlMapTest {
+
+    private UrlMap urls;
+    private String testwww = "www.google.com";
+
     @Before
     public void setUp() throws Exception {
-
+        urls = new UrlMap();
     }
 
     @Test
-    public void addUrl() throws Exception {
+    public void addWWWPrefixUrlReturnTrue() throws Exception {
+        assertTrue(urls.addUrl(testwww));
+    }
 
+    @Test
+    public void addDuplicateUrlReturnFalse() throws Exception {
+        urls.addUrl(testwww);
+        assertFalse(urls.addUrl(testwww));
     }
 
 }
