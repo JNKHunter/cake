82034abc98b9b8e2b2ae3c47e0b0149510c38272
John Hunter <john@pushagency.io>
2016-11-25 17:23:52 -0800

    added trie implementation

--- src/main/java/question11/Main.java
+++ src/main/java/question11/Main.java
@@ -15,10 +15,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        UrlMap urls = new UrlMap();
+        //UrlMap urls = new UrlMap();
 
-        System.out.println(urls.addUrl("www.google.com"));
-        System.out.println(urls.addUrl("www.google.com"));
+        //System.out.println(urls.addUrl("www.google.com"));
+        //System.out.println(urls.addUrl("www.google.com"));
+
+        Trie trie = new Trie();
+
+        trie.checkPresentAndAdd("www.google.com");
+        trie.checkPresentAndAdd("reddit.com");
 
     }
 }
--- src/main/java/question11/Trie.java
+++ src/main/java/question11/Trie.java
@@ -4,4 +4,36 @@ package question11;
  * Created by John on 11/25/16.
  */
 public class Trie {
+
+    private TrieNode rootNode;
+    private final char endOfWordMarker = '\0';
+
+    public Trie() {
+        this.rootNode = new TrieNode();
+    }
+
+    public boolean checkPresentAndAdd(String word){
+        TrieNode currentNode = this.rootNode;
+        boolean isNewWord = false;
+
+        for(int i = 0; i < word.length(); i++){
+
+            char character = word.charAt(i);
+
+            if(!currentNode.hasChildNode(character)){
+                isNewWord = true;
+                currentNode.makeChildNode(character);
+            }
+
+            currentNode = currentNode.getChildNode(character);
+        }
+
+        if (!currentNode.hasChildNode(endOfWordMarker)){
+            isNewWord = true;
+            currentNode.makeChildNode(endOfWordMarker);
+        }
+
+        return isNewWord;
+
+    }
 }
