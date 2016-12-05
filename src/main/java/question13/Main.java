d15deb8b6a1955a48a031b56cb11f3a6c4e251b9
John Hunter <john@push-it-forward.com>
2016-12-04 18:43:41 -0800

    solved

--- src/main/java/question13/Main.java
+++ src/main/java/question13/Main.java
@@ -19,12 +19,34 @@ public class Main {
             "karpatka",
             "othellolagkage"
         };
+
+        System.out.println(findRotationPoint(words));
     }
 
-    public static int getRotationPoint(String[] words){
+    public static int findRotationPoint(String[] words) {
+        final String firstWord = words[0];
+
+        int floorIndex = 0;
+        int ceilingIndex = words.length - 1;
+
+        while (floorIndex < ceilingIndex) {
+
+            int guessIndex = floorIndex + ((ceilingIndex - floorIndex) / 2);
+
+            if (words[guessIndex].compareTo(firstWord) > 0) {
+                floorIndex = guessIndex;
+            } else {
+                ceilingIndex = guessIndex;
+            }
 
 
+            if (floorIndex + 1 == ceilingIndex) {
 
+                break;
+            }
+        }
+
+        return ceilingIndex;
     }
 
 }
