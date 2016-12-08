bf3fe107b3382c69fc5fad650848d0e9dba41c3a
John Hunter <john@push-it-forward.com>
2016-12-07 19:36:24 -0800

    solution

--- src/main/java/question27/Main.java
+++ src/main/java/question27/Main.java
@@ -1,7 +1,31 @@
 package question27;
 
+import java.util.Arrays;
+
 /**
  * Created by jhunter on 12/7/16.
  */
 public class Main {
+
+    public static void main(String[] args) {
+        String message = "find you will pain only go you recordings security the into if";
+        System.out.println(reverseWords(message));
+    }
+
+    public static String reverseWords(String message){
+        String[] sArray = message.split("\\s");
+        int middle = (int)Math.floor(sArray.length/2);
+
+        for(int i = 0; i < middle; i++){
+
+            String former = sArray[i];
+            String latter = sArray[(sArray.length - 1) - i];
+
+            sArray[i] = latter;
+            sArray[(sArray.length - 1) - i] = former;
+
+        }
+
+        return Arrays.toString(sArray);
+    }
 }
