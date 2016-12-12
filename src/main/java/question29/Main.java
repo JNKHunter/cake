5694cf9637ad7a0ad78401095cb3a743895e1c52
John Hunter <john@push-it-forward.com>
2016-12-11 18:58:34 -0800

    added isopener method

--- src/main/java/question29/Main.java
+++ src/main/java/question29/Main.java
@@ -1,5 +1,9 @@
 package question29;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by jhunter on 12/9/16.
  */
@@ -9,6 +13,17 @@ public class Main {
     }
 
     public static boolean validate(String string){
+
+
+        return false;
+    }
+
+    public static boolean isOpener(char c){
+        List<Character> openers = Arrays.asList('[','(','{');
+
+        if (openers.contains(c)) {
+            return true;
+        }
         return false;
     }
 }
