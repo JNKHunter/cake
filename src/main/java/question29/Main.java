240c1a2860c95d7b20fec59650037c303b6744a1
John Hunter <john@push-it-forward.com>
2016-12-11 19:11:34 -0800

    continue validate implementation

--- src/main/java/question29/Main.java
+++ src/main/java/question29/Main.java
@@ -3,6 +3,7 @@ package question29;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * Created by jhunter on 12/9/16.
@@ -15,9 +16,21 @@ public class Main {
     public static boolean validate(String code){
 
         int codeLength = code.length();
+        char currentChar;
+        Stack<Character> blocks = new Stack<>();
+
 
         for(int i = 0; i < codeLength; i++){
+            currentChar = code.charAt(i);
 
+            if(isOpener(currentChar)){
+                blocks.push(currentChar);
+            }
+
+            if(isCloser(currentChar)){
+                char opener = getOpener(currentChar);
+                
+            }
         }
 
         return false;
@@ -32,12 +45,30 @@ public class Main {
         return false;
     }
 
-    public static boolean isCloser(char c){
-        List<Character> openers = Arrays.asList(')',']','}');
+    public static boolean isCloser(char c) {
+        List<Character> openers = Arrays.asList(')', ']', '}');
 
         if (openers.contains(c)) {
             return true;
         }
         return false;
+    }
+
+    public static char getOpener(char c) {
+
+        switch (c){
+
+            case '}':
+                return '{';
+
+            case ']':
+                return '{';
+            case ')':
+                return '(';
+
+            default:
+                return '0';
+        }
+
     }
 }
