b85be367e3c5d2892e3d4f3cfeba05ac532a69a0
John Hunter <john@push-it-forward.com>
2016-12-11 19:02:21 -0800

    started validate implementation

--- src/main/java/question29/Main.java
+++ src/main/java/question29/Main.java
@@ -12,14 +12,28 @@ public class Main {
 
     }
 
-    public static boolean validate(String string){
+    public static boolean validate(String code){
 
+        int codeLength = code.length();
+
+        for(int i = 0; i < codeLength; i++){
+
+        }
 
         return false;
     }
 
     public static boolean isOpener(char c){
         List<Character> openers = Arrays.asList('[','(','{');
+
+        if (openers.contains(c)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isCloser(char c){
+        List<Character> openers = Arrays.asList(')',']','}');
 
         if (openers.contains(c)) {
             return true;
