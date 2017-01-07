5b1a5cf97c35520197d3379379d6e11ad5ba4fb9
John Hunter <john@push-it-forward.com>
2017-01-07 12:16:41 -0800

    added random array generator

--- src/main/java/util/ArrayUtils.java
+++ src/main/java/util/ArrayUtils.java
@@ -1,7 +1,22 @@
 package util;
 
+import java.util.Random;
+
 /**
  * Created by jhunter on 1/7/17.
  */
 public class ArrayUtils {
+
+    public static int[] generateRandomArray(int arraySize, int minValue, int maxValue){
+        int[] theArray = new int[arraySize];
+
+        Random rand = new Random();
+
+        for(int i = 0; i < arraySize; i++){
+            theArray[i] = rand.nextInt((maxValue - minValue) + 1) + minValue;
+        }
+
+        return theArray;
+    }
+
 }
