1cda74b3e5d6c3deb6c7ca72a92ab27a8291fd33
John Hunter <john@push-it-forward.com>
2017-01-09 19:12:07 -0800

    added findduplicate test

--- src/main/java/question33/FindDuplicateInt.java
+++ src/main/java/question33/FindDuplicateInt.java
@@ -4,4 +4,18 @@ package question33;
  * Created by jhunter on 1/9/17.
  */
 public class FindDuplicateInt {
+
+    private int[] values;
+
+    public FindDuplicateInt(int[] values){
+        this.values = values;
+    }
+
+    public int sumAllValues(int length){
+        return ((length * length) + length)/2;
+    }
+
+    public int findDuplicate(){
+        return sumAllValues(values.length) - sumAllValues(values.length - 1);
+    }
 }
---
+++ src/test/java/question33/FindDuplicateIntTest.java
@@ -0,0 +1,10 @@
+package question33;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jhunter on 1/9/17.
+ */
+public class FindDuplicateIntTest {
+
+}
