4712a6b539465a0e2e1703c3666cab409fb462ed
John Hunter <john@push-it-forward.com>
2017-01-11 11:07:10 -0800

    added nodesetnexttest

--- src/test/java/dataStructures/linkedlist/NodeTest.java
+++ src/test/java/dataStructures/linkedlist/NodeTest.java
@@ -1,10 +1,21 @@
 package dataStructures.linkedlist;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by jhunter on 1/11/17.
  */
 public class NodeTest {
+
+    @Test
+    public void nodeSetNextTest(){
+        Node node = new Node(new Object());
+        Node nextNode = new Node(new Object());
+        node.setNext(nextNode);
+
+        assertSame(nextNode, node.getNext());
+    }
 
 }
