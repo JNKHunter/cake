ae27394bb40acae954301e611a579960066b97f3
John Hunter <john@push-it-forward.com>
2017-01-11 10:33:03 -0800

    added node implementation

--- src/main/java/dataStructures/linkedlist/Node.java
+++ src/main/java/dataStructures/linkedlist/Node.java
@@ -4,4 +4,27 @@ package dataStructures.linkedlist;
  * Created by jhunter on 1/11/17.
  */
 public class Node {
+    Node next;
+    Object data;
+
+    public Node(Object data) {
+        this.data = data;
+    }
+
+    public Node(Node next, Object data) {
+        this.next = next;
+        this.data = data;
+    }
+
+    public void setNext(Node nextNode){
+        this.next = nextNode;
+    }
+
+    public Node getNext(){
+        return this.next;
+    }
+
+    public Object getData() {
+        return data;
+    }
 }
