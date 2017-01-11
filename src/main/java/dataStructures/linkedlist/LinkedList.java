3646cf0e1c61e96f16b1c3ada0531147216808b0
John Hunter <john@push-it-forward.com>
2017-01-11 11:04:07 -0800

    added linkedlist implementation. start test

--- src/main/java/dataStructures/linkedlist/LinkedList.java
+++ src/main/java/dataStructures/linkedlist/LinkedList.java
@@ -1,10 +1,95 @@
 package dataStructures.linkedlist;
 
+import java.text.ParseException;
+
 /**
  * Created by jhunter on 1/11/17.
  */
 public class LinkedList {
     private Node head;
+    private int counter;
 
+    public LinkedList(){
+        counter = 0;
+    }
+
+    //Adds a node to the end of the list
+    public void add(Object data){
+
+        if(head != null){
+            Node current = head;
+
+            while(current.getNext() != null){
+                current = current.getNext();
+            }
+            current.setNext(new Node(data));
+        }else{
+            head = new Node(data);
+        }
+
+        counter++;
+    }
+
+    //Adds a node at a particular location in the list
+    public void add(Object data, int index){
+        if (head != null) {
+            Node current = head;
+            for(int i = 0; i < index && current.getNext() != null; i++){
+                current = current.getNext();
+            }
+
+            Node newNode = new Node(data);
+            newNode.setNext(current.getNext());
+            current.setNext(newNode);
+        }else{
+            head = new Node(data);
+        }
+
+        counter++;
+    }
+
+    public Object get(int index){
+        if(index < 0){
+            return null;
+        }
+
+        if(head != null){
+            Node current = head;
+            for(int i = 0; i < index; i++){
+                if(current.getNext() == null){
+                    return null;
+                }
+
+                current.getNext();
+            }
+
+            return current.getData();
+        }
+
+        return null;
+
+    }
+
+    public boolean remove(int index){
+        if(index < 1 || index > counter){
+            return false;
+        }
+
+        if(head != null){
+            Node current = head;
+            for(int i = 0; i < index; i++){
+                if(current.getNext() == null){
+                    return false;
+                }
+                current = current.getNext();
+            }
+
+            current.setNext(current.getNext().getNext());
+            counter--;
+            return true;
+        }
+
+        return false;
+    }
 
 }
---
+++ src/test/java/dataStructures/linkedlist/NodeTest.java
@@ -0,0 +1,10 @@
+package dataStructures.linkedlist;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jhunter on 1/11/17.
+ */
+public class NodeTest {
+
+}
