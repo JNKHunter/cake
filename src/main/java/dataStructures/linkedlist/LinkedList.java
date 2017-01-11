9c6bdf1e33a5488da6b03b77ee9a52b0e782bf01
John Hunter <john@push-it-forward.com>
2017-01-11 10:28:34 -0800

    added linkedlist and node

---
+++ src/main/java/dataStructures/linkedlist/LinkedList.java
@@ -0,0 +1,10 @@
+package dataStructures.linkedlist;
+
+/**
+ * Created by jhunter on 1/11/17.
+ */
+public class LinkedList {
+    private Node head;
+
+
+}
---
+++ src/main/java/dataStructures/linkedlist/Node.java
@@ -0,0 +1,7 @@
+package dataStructures.linkedlist;
+
+/**
+ * Created by jhunter on 1/11/17.
+ */
+public class Node {
+}
