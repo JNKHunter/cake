ab37f1e56d37089d5c8035627dd77fb16b3f0d9b
John Hunter <john@push-it-forward.com>
2017-02-07 18:55:13 -0800

    added cake type

--- src/main/java/question16/CakeType.java
+++ src/main/java/question16/CakeType.java
@@ -4,4 +4,27 @@ package question16;
  * Created by jhunter on 2/7/17.
  */
 public class CakeType {
+    private int weight;
+    private int value;
+
+    public CakeType(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
 }
