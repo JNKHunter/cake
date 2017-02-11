4c5ca08609f3f486bc9c0fe49b2b4bb58137e781
John Hunter <john@push-it-forward.com>
2017-02-10 16:12:00 -0800

    added tests for weighted quick union

--- src/main/java/algs4/SocialConnectivity.java
+++ src/main/java/algs4/SocialConnectivity.java
@@ -4,4 +4,6 @@ package algs4;
  * Created by jhunter on 2/10/17.
  */
 public class SocialConnectivity {
+
+    //Use a weighted quick union to determine when all users in a social network are connected
 }
---
+++ src/test/java/util/WeightedQuickUnionTest.java
@@ -0,0 +1,26 @@
+package util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jhunter on 2/10/17.
+ */
+public class WeightedQuickUnionTest {
+    @Test
+    public void count() throws Exception {
+
+    }
+
+    @Test
+    public void find() throws Exception {
+
+    }
+
+    @Test
+    public void connected() throws Exception {
+
+    }
+
+}
