ce6eac694377d4f9857359ecf75b53915c500961
John Hunter <john@pushagency.io>
2017-06-27 07:31:37 -0400

    solved

--- src/main/java/question1/Main.java
+++ src/main/java/question1/Main.java
@@ -4,23 +4,23 @@ package question1;
  * Created by John on 11/8/16.
  */
 public class Main {
-
     public static void main(String[] args) {
-        int[] stockPricesYesterday = new int[]{10, 7, 5, 8, 11, 9, 12, 1, 15};
-        System.out.println(getMaxProfit(stockPricesYesterday));
+        int[] stockPricesYesterday = new int[] {10, 7, 5, 8, 11, 9};
+        getMaxProfit(stockPricesYesterday);
     }
 
-    public static int getMaxProfit(int[] prices){
-        int minPrice = prices[0];
-        int maxProfit = 0;
+    private static void getMaxProfit(int[] prices) {
+        int leftPointer = 0;
+        int rightPointer = prices.length - 1;
+        int smallestLoss = leftPointer - rightPointer;
 
-        for(int currentPrice: prices){
-            minPrice = Math.min(minPrice, currentPrice);
-            int potentialProfit = currentPrice - minPrice;
-            maxProfit = Math.max(maxProfit, potentialProfit);
+        while(leftPointer < rightPointer + 2)
+        if(smallestLoss <  prices[leftPointer] - prices[rightPointer - 1]) {
+            rightPointer -= 1;
+        } else if (smallestLoss < prices[leftPointer + 1] - prices[rightPointer]) {
+            leftPointer += 1;
         }
 
-        return maxProfit;
     }
 
 }
---
+++ src/main/java/question4/Main.java
@@ -0,0 +1,84 @@
+package question4;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by John on 6/27/17.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        List<Meeting> today = new ArrayList<>();
+        today.add(new Meeting(4,6));
+        today.add(new Meeting(2,5));
+        today.add(new Meeting(2,4));
+        today.add(new Meeting(3,5));
+        today.add(new Meeting(9,10));
+        today.add(new Meeting(10,11));
+        today.add(new Meeting(12,13));
+        today.add(new Meeting(12,11));
+
+        mergeMeetings(today);
+
+    }
+
+    public static List<Meeting> mergeMeetings(List<Meeting> meetings) {
+
+        meetings.sort(new Comparator<Meeting>() {
+            @Override
+            public int compare(Meeting o1, Meeting o2) {
+                if(o1.getStartTime() >= o2.getStartTime()) {
+                    if(o1.getStartTime() == o2.getStartTime()) {
+                        if(o1.getEndTime() > o2.getEndTime()) {
+                            return 1;
+                        } else if (o1.getEndTime() < o2.getEndTime()) {
+                            return -1;
+                        } else {
+                            return 0;
+                        }
+                    }
+                    return 1;
+                }
+
+                if(o1.getStartTime() < o2.getStartTime()) {
+                    return -1;
+                }
+                return 0;
+            }
+        });
+
+        for(int k = 0; k < meetings.size(); k ++) {
+            System.out.println(meetings.get(k).toString());
+        }
+
+        System.out.println("--------------------");
+
+        List<Meeting> mergedMeetings = new ArrayList<>();
+
+        for(int i = 0; i < meetings.size(); i++) {
+
+            Meeting mergedMeeting = new Meeting(meetings.get(i).getStartTime(),
+                    meetings.get(i).getEndTime());
+
+            mergedMeetings.add(mergedMeeting);
+
+            for(int j = i+1; j < meetings.size(); j++) {
+                if(mergedMeeting.getEndTime() >= meetings.get(j).getStartTime()) {
+                    mergedMeeting.setEndTime(meetings.get(j).getEndTime());
+                } else {
+                    i = j-1;
+                    break;
+                }
+            }
+
+
+        }
+
+        for(int k = 0; k < mergedMeetings.size(); k ++) {
+            System.out.println(mergedMeetings.get(k).toString());
+        }
+        return mergedMeetings;
+    }
+}
